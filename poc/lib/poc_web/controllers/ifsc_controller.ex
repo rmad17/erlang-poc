ac041e2795906afcc9f91d14c6d875f43c7750a6
Sourav Basu <sourav@reckonsys.com>
2019-10-21 00:24:17 +0530

    Added insert statement

--- poc/lib/poc_web/controllers/ifsc_controller.ex
+++ poc/lib/poc_web/controllers/ifsc_controller.ex
@@ -9,6 +9,7 @@ defmodule PocWeb.IfscController do
     url = "https://ifsc.razorpay.com/" <> _params["ifsc_code"]
     response = HTTPoison.get!(url)
     body = Poison.decode!(response.body)
+    Poc.Repo.insert(%Poc.IfscDetail{"ifsc": body["ifsc"], "bank": body["bankcode"], "branch": body["branch"], "address": body["address"]})
     conn
     |> json(body)
   end
